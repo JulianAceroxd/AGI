d01d386b88bd117518665ff8c139c9cd7cf3ddea
gestdocagi <112114160+gestdocagi@users.noreply.github.com>
2022-09-24 11:19:30 -0500

    "Subiendo Proyecto"

--- Controlador/Controller.js
+++ Controlador/Controller.js
@@ -12,30 +12,46 @@ Controller.index = (req, res, next) => {
 };
 
 Controller.cuatro = (req, res) => {
- res.render('404')
+  res.render('404')
 
 };
 
 
 Controller.admin = (req, res, next) => {
-    if(req.session.Login){
-    res.render("admin",{
-        login:true,
-        name: req.session.nombre
+  if (req.session.Login) {
+    res.render("admin", {
+      login: true,
+      name: req.session.nombre
     });
-}else{
-    console.log("DATOS INCORRECTOS"); //SALIMOS DEL IF DE ENTRADA Y SWITCH A UN VALIDADOR SI LOS DATOS SON INCORRECTOS
-    res.render("Login",{
-        alert:true,
-        alertTitle:"Error",
-        alertMessage: "DEBE INICIAR SESION",
-        alertIcon: "error",
-        showConfirmButton: true,
-        timer: 2000,
-        ruta: '/'
-    });
-}
-  };
+  } else {
+    res.render("Login", {
+      alert: true,
+      alertTitle: "Error",
+      alertMessage: "DEBE INICIAR SESION",
+      alertIcon: "error",
+      showConfirmButton: true,
+      timer: 2000,
+      ruta: '/'
+    }); //NOS RE
+
+  }
+};
+Controller.clientes = (req, res, next) => {
+
+  res.render("Clientes", {
+    login: true,
+    name: req.session.nombre
+  });
+
+};
+Controller.Variedades = (req, res, next) => {
+
+  res.render("Variedades", {
+    login: true,
+    name: req.session.nombre
+  });
+
+};
 
 Controller.Logine = async (req, res, next) => {
   //LOGINN
@@ -48,13 +64,13 @@ Controller.Logine = async (req, res, next) => {
     async (err, results) => {
       //CONSULTAMOS LOS DATOS EN LA BASE DE DATOS Y REEMPLAZAMOS VALORES CON LOS QUE DILIGENCIA EL USUARIO
       if (err) {
-        next(new Error(res.redirect('/'),console.log("ERROR AL REALIZAR LA CONSULTA"), err)); //VALIDAMOS SI EXITEN ERRORES
-      } else if (results != 0 && (await (cla, results[0].Clave))) {
+        next(new Error(res.redirect('/'), console.log("ERROR AL REALIZAR LA CONSULTA"), err)); //VALIDAMOS SI EXITEN ERRORES
+      } else if (results != 0 && (await (cla, results[0].Password))) {
         // SI EL RESULTADO ES DIFERENTE DE 0 ES QUE ENCONTRO EL USUARIO,POR MEDIO DE UN ARREGLO Y COMPARE, COMPARAMOS LO DILIGENCIADO POR EL USUARIO Y LO REGISTRADO EN LA BD                           console.log("Datos Correctossssssss");
 
         //CREAMOS SESIONES POR MEDIO DE UN ARREGLO, QUE NOS RETORNA LOS DATOS DE EL USUARIO LOGEADO
         req.session.Login = true;
-       const Nombre = results[0].Nombre;
+        const Nombre = results[0].Nombre;
         Apellido = results[0].Apellido;
 
         Rol = results[0].Rol;
@@ -62,15 +78,15 @@ Controller.Logine = async (req, res, next) => {
 
         switch (Rol) {
           case 1:
-           req.session.nombre = Nombre;
-            res.render('Login',{
-                alert:true,
-                alertTitle:"Conexion Exitosa",
-                alertMessage: "Bienvenido "+ req.session.nombre,
-                alertIcon: "success",
-                showConfirmButton: true,
-                timer: 2500,
-                ruta: 'Admin'
+            req.session.nombre = Nombre;
+            res.render('Login', {
+              alert: true,
+              alertTitle: "Conexion Exitosa",
+              alertMessage: "Bienvenido " + req.session.nombre,
+              alertIcon: "success",
+              showConfirmButton: true,
+              timer: 2500,
+              ruta: 'Admin'
             });
             break;
           case "2":
@@ -81,14 +97,14 @@ Controller.Logine = async (req, res, next) => {
         }
       } else {
         console.log("DATOS INCORRECTOS"); //SALIMOS DEL IF DE ENTRADA Y SWITCH A UN VALIDADOR SI LOS DATOS SON INCORRECTOS
-        res.render("Login",{
-            alert:true,
-            alertTitle:"Error",
-            alertMessage: "Usuario O Clave Incorrecto",
-            alertIcon: "error",
-            showConfirmButton: true,
-            timer: 2000,
-            ruta: '/'
+        res.render("Login", {
+          alert: true,
+          alertTitle: "Error",
+          alertMessage: "Usuario O Clave Incorrecto",
+          alertIcon: "error",
+          showConfirmButton: true,
+          timer: 2000,
+          ruta: '/'
         }); //NOS REDIRIGE AL MISMO ARCHIVO
       }
     }
@@ -97,9 +113,13 @@ Controller.Logine = async (req, res, next) => {
 
 
 Controller.cerrar = (req, res, next) => {
-req.session.destroy(()=>{
-  res.redirect('/')
-})
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
 };
+
+
+
+
 
 module.exports = Controller;
--- Rutas/Rutas.js
+++ Rutas/Rutas.js
@@ -6,6 +6,8 @@ const Rutas=express.Router();
 Rutas.get('/',Controller.index); 
 Rutas.get('/Admin',Controller.admin); 
 Rutas.get('/cerrar',Controller.cerrar); 
+Rutas.get('/Clientes',Controller.clientes); 
+Rutas.get('/Variedades',Controller.Variedades); 
 
 Rutas.post('/Logine',Controller.Logine); 
 
